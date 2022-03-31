4a4a4c57206a97b250d33b784158bbd65e3e2d01
ericpkatz <ericpkatz@gmail.com>
2022-03-31 19:43:31 -0400

    set up get route

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+const indexPath = path.join(__dirname, 'index.html');
+
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
 
 const port = process.env.PORT || 3000;
 
