d1dd1a13c1f5efe6ef8f7db938fccedef2beaf5d
ericpkatz <ericpkatz@gmail.com>
2022-03-31 20:37:24 -0400

    added models

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ const indexPath = path.join(__dirname, 'index.html');
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
+app.get('/api/tasks', async(req, res, next)=> {
+  try {
+    res.send(await Task.findAll());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/users', async(req, res, next)=> {
+  try {
+    res.send(await User.findAll());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
 
 const port = process.env.PORT || 3000;
 
@@ -16,6 +34,18 @@ app.listen(port, ()=> console.log(`listening on port ${port}`));
 const init = async()=> {
   console.log('calling init');
   await sequelize.sync({ force: true });
+  await Promise.all([
+    User.create({ firstName: 'lucy' }),
+    User.create({ firstName: 'moe' }),
+    User.create({ firstName: 'ethyl' }),
+  ]);
+
+  await Promise.all([
+    Task.create({ name: 'buy milk'}),
+    Task.create({ name: 'walk dog'}),
+    Task.create({ name: 'work out'}),
+    Task.create({ name: 'call Fred'}),
+  ]);
 };
 
 init();
