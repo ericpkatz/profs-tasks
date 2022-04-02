20a92d79364c6f0af2bee590bf3400d5ad4b66ff
ericpkatz <ericpkatz@gmail.com>
2022-04-02 09:54:25 -0400

    using webpack

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
