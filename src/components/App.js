dd5a786524ca363673b4f53a7a46be81c4ce537f
ericpkatz <ericpkatz@gmail.com>
2022-04-04 20:44:54 -0400

    users without tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component} from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
 import Home from './Home';
 import Users from './Users';
+import UsersWithoutTasks from './UsersWithoutTasks';
 import Tasks from './Tasks';
 import UserTasks from './UserTasks';
 
@@ -16,10 +17,13 @@ class App extends Component{
     return (
       <div>
         <Nav />
-        <Route path='/' exact component={ Home } />
-        <Route path='/users' exact component={ Users } />
-        <Route path='/users/:id' component={ UserTasks } />
-        <Route path='/tasks' component={ Tasks } />
+        <Switch>
+          <Route path='/' exact component={ Home } />
+          <Route path='/users' exact component={ Users } />
+          <Route path='/users/without-tasks' component={ UsersWithoutTasks } />
+          <Route path='/users/:id' component={ UserTasks } />
+          <Route path='/tasks' component={ Tasks } />
+        </Switch>
       </div>
     );
   }
